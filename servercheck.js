44f898e0ca3a8b9a0592f97418b769bec3e42c83
Miguel Gonzalez <miguel-gonzalez@gmx.de>
2013-11-19 02:07:01 +0100

    Added responsive design

--- servercheck.js
+++ servercheck.js
@@ -6,36 +6,38 @@
  * Created: 2013-11-19
  */
 
+var server = 'http://renerisha.no-ip.biz:25565';
+
 function setStatus(status) {
 
  	var button = $('.server_button');
 
- 	button.removeClass(status + 'offline');
+ 	button.removeClass('online offline');
  	button.addClass(status);
 
  	// Change text
  	button.children('.status').html(status);
  }
 
- function checkStatus() {
- 	$.ajax({
-		type: 'GET',
-		url: 'http://renerisha.no-ip.biz',
-		timeout: 15000,
-		success: function(data) { 
-			setStatus('online'); 
-		},
-		error: function(XMLHttpRequest, textStatus, errorThrown) {
-			setStatus('offline'); 
-		}
-	});
- }
+function pingServer(ip) {
+
+    $.ajax({
+        url: ip,
+        timeout: 3000, //set the timeout to 3 seconds
+        success: function(){
+            setStatus('online');
+        },
+        error: function(req, err){
+           setStatus('online');
+        }
+    });
+}
 
  $(document).ready(function() {
- 	checkStatus();
+ 	pingServer(server);
  });
 
 window.setInterval(function() {
-	checkStatus();
+	pingServer(server);
 }, 10000);
 
