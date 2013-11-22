9bda7bcb1c9df404b34c5524353ce8e72e8412c2
Miguel Gonzalez <miguel-gonzalez@gmx.de>
2013-11-22 03:04:06 +0100

    Added content handling

---
+++ content.js
@@ -0,0 +1,95 @@
+/* 
+ * File which checks for the server
+ *
+ * Author:  Miguel Gonzalez <miguel-gonzalez@gmx.de>
+ * Version: 1.0
+ * Created: 2013-11-19
+ */
+
+function ContentHandler() {
+
+    this.ip = "127.0.0.1";
+    this.port = "25566";
+    this.name = "mineweb";
+    this.website = "https://github.com/MyRealityCoding/mineweb";
+    this.forum = "https://github.com/MyRealityCoding/mineweb";
+    this.map = "https://github.com/MyRealityCoding/mineweb";
+
+    var content = this;
+
+    $.ajax({
+                type: "GET",
+                url: "config.json",
+                dataType: "json",
+                crossDomain: true,
+                async: false,
+                success: function(data) {
+                    content.ip = data['ip'];
+                    content.port = data['port'];
+                    content.name = data['name'];
+                    content.website = data['website'];
+                    content.forum = data['forum'];
+                    content.map = data['map'];
+                }
+     });
+
+
+}
+
+var content = new ContentHandler();
+
+var space = $('<div></div>');
+
+function setStatus(status) {
+
+ 	var button = $('.server_button');
+
+ 	button.removeClass('online offline');
+ 	button.addClass(status);
+
+ 	// Change text
+ 	button.children('.status').html(status);
+	space.hide();
+	button.fadeIn();
+
+	
+ }
+
+function update() {
+	$.ajax({
+                type: "GET",
+                url: "proxy.php?url=http://" + content.ip + ":" + content.port,
+                dataType: "json",
+                crossDomain: true,
+                async: true,
+                timeout: 1000,
+                success: function(data) {
+                    setStatus('online');
+                },
+                error: function(req, err) {
+                	setStatus('offline');
+                }
+            });
+}
+
+function setContent() {
+    $('.ip').html(content.ip);
+    $('.server_button').attr('href', content.website);
+    $('.banner').html(content.name);
+}
+
+ $(document).ready(function() {
+	var button = $('.server_button');
+	var height = button.outerHeight();
+	space.height(height);
+	space.insertAfter(button);	
+	button.hide();
+    setContent();
+ 	update();
+
+ });
+
+window.setInterval(function() {
+	update();
+}, 10000);
+
